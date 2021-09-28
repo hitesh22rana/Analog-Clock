d33b63cca0a03d78f9dcaa829f9f8c8f4b68c879
hitesh <hitesh22rana@gmail.com>
2021-09-29 00:16:12 +0530

    added ticking audio sound

--- script.js
+++ script.js
@@ -3,8 +3,25 @@ const hr = document.getElementById('hr');
 const min = document.getElementById('min');
 const sec = document.getElementById('sec');
 
-setInterval(() => {
+let audio = new Audio('./clock-sounds/clock-ticking.mp3');
+
+function audioloop (audio) {
+
+    if (typeof audio.loop == 'boolean') {
+        audio.loop = true;
+    }
     
+    else {
+        audio.addEventListener('ended', function() {
+            this.currentTime = 0;
+            this.play();
+        }, false);
+    }
+    audio.play();
+}
+
+setInterval(() => {   
+    audioloop(audio);
     let day = new Date();
     let hh = day.getHours() * 30;
     let mn = day.getMinutes() * deg;
