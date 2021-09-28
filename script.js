ce1ad4582b5eb630c17f74e53abc33c996753782
hitesh <hitesh22rana@gmail.com>
2021-09-29 00:41:09 +0530

    clock sound error and delay fixed

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ const hr = document.getElementById('hr');
 const min = document.getElementById('min');
 const sec = document.getElementById('sec');
 
-let audio = new Audio('./clock-sounds/clock-ticking.mp3');
+let audio = document.getElementById('tick-tick');
+
+setInterval(() => {   
+    
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mn = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+    
+    audioloop(audio);
+
+    hr.style.transform = `rotateZ(${hh+(mn/12)}deg)`;
+    min.style.transform = `rotateZ(${mn}deg)`;
+    sec.style.transform = `rotateZ(${ss}deg)`;
+
+});
+
 
 function audioloop (audio) {
 
@@ -17,18 +33,14 @@ function audioloop (audio) {
             this.play();
         }, false);
     }
-    audio.play();
-}
+    let resp = audio.play();
 
-setInterval(() => {   
-    audioloop(audio);
-    let day = new Date();
-    let hh = day.getHours() * 30;
-    let mn = day.getMinutes() * deg;
-    let ss = day.getSeconds() * deg;
+    if (resp!== undefined) {
+        resp.then(_ => {
+            // autoplay starts!
+        }).catch(error => {
+        //show error
+        });
+    }
     
-    hr.style.transform = `rotateZ(${hh+(mn/12)}deg)`;
-    min.style.transform = `rotateZ(${mn}deg)`;
-    sec.style.transform = `rotateZ(${ss}deg)`;
-
-});
+}
